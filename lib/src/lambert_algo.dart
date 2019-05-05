16b56150c78525f5472a9a673173b1f0108d2dad
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-05-05 15:58:29 +0200

    Update for dart 2

--- lib/src/lambert_algo.dart
+++ lib/src/lambert_algo.dart
@@ -5,32 +5,34 @@ import 'dart:core';
 import 'lambert_point.dart' as pt;
 import 'lambert_zone.dart' as lambertZone;
 
-double latitudeISOFromLatitude(double lat, double e)
-{
-  return log(tan(PI/4+lat/2)*pow((1-e*sin(lat))/(1+e*sin(lat)),e/2));
+double latitudeISOFromLatitude(double lat, double e) {
+  return log(tan(pi / 4 + lat / 2) *
+      pow((1 - e * sin(lat)) / (1 + e * sin(lat)), e / 2));
 }
 
-double latitudeFromLatitudeISO(double lat_iso, double e,double eps)
-{
-
-  double phi_0 =  2*atan(exp(lat_iso)) - PI/2;
-  double phi_i = 2*atan(pow((1+e*sin(phi_0))/(1-e*sin(phi_0)),e/2.0)*exp(lat_iso)) - PI/2;
+double latitudeFromLatitudeISO(double lat_iso, double e, double eps) {
+  double phi_0 = 2 * atan(exp(lat_iso)) - pi / 2;
+  double phi_i = 2 *
+          atan(pow((1 + e * sin(phi_0)) / (1 - e * sin(phi_0)), e / 2.0) *
+              exp(lat_iso)) -
+      pi / 2;
   double delta = phi_i - phi_0;
 
-  while(delta.abs() > eps)
-  {
+  while (delta.abs() > eps) {
     phi_0 = phi_i;
-    phi_i = 2*atan(pow((1+e*sin(phi_0))/(1-e*sin(phi_0)),e/2.0)*exp(lat_iso)) - PI/2;
+    phi_i = 2 *
+            atan(pow((1 + e * sin(phi_0)) / (1 - e * sin(phi_0)), e / 2.0) *
+                exp(lat_iso)) -
+        pi / 2;
 
     delta = phi_i - phi_0;
   }
 
   return phi_i;
-
 }
 
-pt.Point lambertToGeographic(pt.Point org, lambertZone.Zone zone, double lon_merid, double e, double eps) {
-
+pt.Point lambertToGeographic(pt.Point org, lambertZone.Zone zone,
+    double lon_merid, double e, double eps) {
   double n = lambertZone.N(zone);
   double C = lambertZone.C(zone);
   double x_s = lambertZone.Xs(zone);
@@ -54,48 +56,59 @@ pt.Point lambertToGeographic(pt.Point org, lambertZone.Zone zone, double lon_mer
   return new pt.Point(lon, lat, org.z);
 }
 
-double lambertNormal(double lat, double a, double e)
-{
-
-  return a/sqrt(1-e*e*sin(lat)*sin(lat));
+double lambertNormal(double lat, double a, double e) {
+  return a / sqrt(1 - e * e * sin(lat) * sin(lat));
 }
 
+pt.Point geographicToCartesian(
+    double lon, double lat, double he, double a, double e) {
+  double N = lambertNormal(lat, a, e);
 
-pt.Point geographicToCartesian(double lon, double lat, double he, double a, double e)
-{
-  double N = lambertNormal(lat,a,e);
+  pt.Point point = new pt.Point(0.0, 0.0, 0.0);
+  point.x = (N + he) * cos(lat) * cos(lon);
 
-  pt.Point point = new pt.Point(0.0,0.0,0.0);
-  point.x = (N+he)*cos(lat)*cos(lon);
+  point.y = (N + he) * cos(lat) * sin(lon);
 
-  point.y = (N+he)*cos(lat)*sin(lon);
-
-  point.z = (N*(1-e*e)+he)*sin(lat);
+  point.z = (N * (1 - e * e) + he) * sin(lat);
 
   return point;
 }
 
-pt.Point cartesianToGeographic(pt.Point org, double meridien, double a, double e , double eps)
-{
+pt.Point cartesianToGeographic(
+    pt.Point org, double meridien, double a, double e, double eps) {
   double x = org.x, y = org.y, z = org.z;
 
-  double lon = meridien + atan(y/x);
+  double lon = meridien + atan(y / x);
 
-  double module = sqrt(x*x + y*y);
+  double module = sqrt(x * x + y * y);
 
-  double phi_0 = atan(z/(module*(1-(a*e*e)/sqrt(x*x+y*y+z*z))));
-  double phi_i = atan(z/module/(1-a*e*e*cos(phi_0)/(module * sqrt(1-e*e*sin(phi_0)*sin(phi_0)))));
+  double phi_0 =
+      atan(z / (module * (1 - (a * e * e) / sqrt(x * x + y * y + z * z))));
+  double phi_i = atan(z /
+      module /
+      (1 -
+          a *
+              e *
+              e *
+              cos(phi_0) /
+              (module * sqrt(1 - e * e * sin(phi_0) * sin(phi_0)))));
   double delta = phi_i - phi_0;
-  while(delta.abs() > eps)
-  {
+  while (delta.abs() > eps) {
     phi_0 = phi_i;
-    phi_i = atan(z/module/(1-a*e*e*cos(phi_0)/(module * sqrt(1-e*e*sin(phi_0)*sin(phi_0)))));
+    phi_i = atan(z /
+        module /
+        (1 -
+            a *
+                e *
+                e *
+                cos(phi_0) /
+                (module * sqrt(1 - e * e * sin(phi_0) * sin(phi_0)))));
 
     delta = phi_i - phi_0;
   }
 
-  double he = module/cos(phi_i) - a/sqrt(1-e*e*sin(phi_i)*sin(phi_i));
+  double he =
+      module / cos(phi_i) - a / sqrt(1 - e * e * sin(phi_i) * sin(phi_i));
 
   return new pt.Point(lon, phi_i, he);
 }
-
--- test/lambert_test.dart
+++ test/lambert_test.dart
@@ -1,4 +1,3 @@
-
 library lambert.test;
 
 import 'package:lambert/lambert.dart' as lambert;
@@ -6,27 +5,25 @@ import 'package:test/test.dart';
 
 void main() {
   group('Valid elements', () {
-
     test('Lambert93', () {
-
-      lambert.Point point = new lambert.Point(668832.5384,6950138.7285,0.0);
+      lambert.Point point = new lambert.Point(668832.5384, 6950138.7285, 0.0);
       lambert.Point expected = new lambert.Point(2.56865, 49.64961, 0.0);
 
       lambert.Point result = point.convert(lambert.Zone.L93).degree();
 
-      expect((result.x - expected.x),lessThanOrEqualTo(1e-5));
-      expect((result.y - expected.y),lessThanOrEqualTo(1e-5));
+      expect((result.x - expected.x), lessThanOrEqualTo(1e-5));
+      expect((result.y - expected.y), lessThanOrEqualTo(1e-5));
     });
 
     test('ZenithStrasbourg', () {
-
-      lambert.Point point = new lambert.Point(994300.623,113409.981,0.0);
-      lambert.Point expected = new lambert.Point(7.68639475277068, 48.5953456709144, 0.0);
+      lambert.Point point = new lambert.Point(994300.623, 113409.981, 0.0);
+      lambert.Point expected =
+          new lambert.Point(7.68639475277068, 48.5953456709144, 0.0);
 
       lambert.Point result = point.convert(lambert.Zone.I).degree();
 
-      expect((result.x - expected.x),lessThanOrEqualTo(1e-5));
-      expect((result.y - expected.y),lessThanOrEqualTo(1e-5));
+      expect((result.x - expected.x), lessThanOrEqualTo(1e-5));
+      expect((result.y - expected.y), lessThanOrEqualTo(1e-5));
     });
   });
 }
