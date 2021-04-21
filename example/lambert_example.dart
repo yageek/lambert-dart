b840dd927690fc7ad03f426a14ad8bf41e867446
Yannick Heinrich <yannick.heinrich@gmail.com>
2021-04-21 12:01:22 +0200

    Prepare for null-safety

--- example/lambert_example.dart
+++ example/lambert_example.dart
@@ -6,9 +6,7 @@ library lambert.example;
 import 'package:lambert/lambert.dart' as lambert;
 
 main() {
-  lambert.Point point = new lambert.Point(668832.5384, 6950138.7285, 0.0);
-  lambert.Point expected = new lambert.Point(2.56865, 49.64961, 0.0);
-
+  var point = new lambert.Point(668832.5384, 6950138.7285, 0.0);
   lambert.Point result = point.convert(lambert.Zone.L93).degree();
 
   print("Result: ${result.y} ${result.x}");
