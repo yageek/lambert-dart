0958db863d9f187a619aede14f725cf9685000b3
Yannick Heinrich <yannick.heinrich@gmail.com>
2021-04-21 11:49:18 +0200

    Remove unused dependencies

--- lib/lambert.dart
+++ lib/lambert.dart
@@ -1,12 +1,10 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2016, Yannick Heinrich. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 /// The lambert library.
 ///
 /// This is an awesome library. More dartdocs go here.
 library lambert;
-
-// TODO: Export any libraries intended for clients of this package.
 
 export 'src/lambert_zone.dart';
 export 'src/lambert_point.dart';
